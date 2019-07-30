e2531897cc976af9877059bf7d5382ac620b7e48
Alessandro Decina <alessandro.d@gmail.com>
2019-07-30 10:43:42 -0400

    Look for kernel headers in /lib/modules/`uname -r`/build

--- redbpf/src/build/headers.rs
+++ redbpf/src/build/headers.rs
@@ -3,8 +3,10 @@ use regex::Regex;
 use std::env;
 use std::ffi::OsString;
 use std::process::Command;
+use std::path::Path;
 
 use crate::build::Error;
+use crate::uname;
 
 pub const KERNEL_HEADERS: [&'static str; 6] = [
     "arch/x86/include",
@@ -16,7 +18,7 @@ pub const KERNEL_HEADERS: [&'static str; 6] = [
 ];
 
 pub fn headers() -> Result<Vec<OsString>, Error> {
-    let mut headers_base_path = env_kernel_path().or_else(|_| arch_kernel_path())?;
+    let mut headers_base_path = env_kernel_path().or_else(|_| lib_modules_kernel_path())?;
     if !headers_base_path.ends_with("/") {
         headers_base_path.push('/');
     }
@@ -31,53 +33,15 @@ pub fn env_kernel_path() -> Result<String, Error> {
     env::var("KERNEL_SOURCE").map_err(|_| Error::KernelHeadersNotFound)
 }
 
-pub fn arch_kernel_path() -> Result<String, Error> {
-    let pacman = Command::new("pacman")
-        .args(vec!["-Ql", "linux-headers"])
-        .output()
-        .map_err(|_| Error::OSUnsupported)?;
+pub fn lib_modules_kernel_path() -> Result<String, Error> {
+    if let Ok(u) = uname::uname() {
+        let release = uname::to_str(&u.release);
+        let path = format!("/lib/modules/{}/build/", release);
+        if Path::new(&format!("{}Kconfig", path)).is_file() {
+            return Ok(path);
+        }
 
-    if !pacman.status.success() {
-        return Err(Error::KernelHeadersNotFound);
     }
 
-    arch_filter_output(&String::from_utf8(pacman.stdout).unwrap())
-}
-
-fn arch_filter_output(output: &str) -> Result<String, Error> {
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"(?m)^linux-headers (/usr/lib/modules/(.*)/build/)$").unwrap();
-    }
-
-    let maybe = RE
-        .captures_iter(&output)
-        .next()
-        .ok_or(Error::InvalidOutput)?;
-
-    Ok(maybe[1].to_string())
-}
-
-mod test {
-    #[test]
-    fn test_arch_output() {
-        use crate::build::arch_filter_output;
-        assert_eq!(
-            arch_filter_output(
-                r"
-linux-headers /usr/
-linux-headers /usr/lib/
-linux-headers /usr/lib/modules/
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/.config
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/.tmp_versions/
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/Kconfig
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/Makefile
-linux-headers /usr/lib/modules/4.17.2-1-ARCH/build/Module.symvers
-"
-            ).unwrap(),
-            "/usr/lib/modules/4.17.2-1-ARCH/build/"
-        )
-    }
+    Err(Error::KernelHeadersNotFound)
 }
