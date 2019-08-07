901e6467f75159d19682a0c09de1be051acb407c
Alessandro Decina <alessandro.d@gmail.com>
2019-08-07 05:14:08 -0400

    Add option to specify the kernel version with $KERNEL_VERSION

--- bpf-sys/src/headers.rs
+++ bpf-sys/src/headers.rs
@@ -8,14 +8,20 @@ pub fn kernel_headers_path() -> Option<String> {
 }
 
 fn lib_modules_kernel_path() -> Option<String> {
-    if let Ok(u) = uname::uname() {
-        let release = uname::to_str(&u.release);
-        let path = format!("/lib/modules/{}/build/", release);
+    if let Some(version) = kernel_version() {
+        let path = format!("/lib/modules/{}/build/", version);
         if Path::new(&format!("{}Kconfig", path)).is_file() {
             return Some(path);
         }
-
     }
 
     None
+}
+
+fn kernel_version() -> Option<String> {
+    env::var("KERNEL_VERSION").ok().or_else(|| {
+        uname::uname().ok().map(|u| {
+            uname::to_str(&u.release).to_string()
+        })
+    })
 }
