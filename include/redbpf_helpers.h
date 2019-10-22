bc3fb40665369764d2b1b721e4837d224c7affa6
Alessandro Decina <alessandro.d@gmail.com>
2019-10-22 12:43:51 +0000

    Update CI
    Build on ubuntu with kernels 4.15 and 5.0

--- include/redbpf_helpers.h
+++ include/redbpf_helpers.h
@@ -1,5 +1,9 @@
 #include <linux/kconfig.h>
 #include <linux/types.h>
+#ifdef asm_volatile_goto
+#undef asm_volatile_goto
+#define asm_volatile_goto(x...) asm volatile("invalid use of asm_volatile_goto")
+#endif
 #include <linux/version.h>
 #include <uapi/linux/ptrace.h>
 #include <linux/bpf.h>
