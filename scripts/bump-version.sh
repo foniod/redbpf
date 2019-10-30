1571841b4ac8cb72bb25e0363be28dfe7fcdf7c0
Alessandro Decina <alessandro.d@gmail.com>
2019-10-31 10:19:01 +1100

    Add scripts/bump-version.sh

---
+++ scripts/bump-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+PACKAGES="bpf-sys redbpf redbpf-probes redbpf-macros cargo-bpf"
+
+NEW_VERSION="${1}"
+
+echo "Bumping version: ${NEW_VERSION}"
+
+find $PACKAGES -name Cargo.toml -type f -exec sed -i -e "s/^version.*/version = \"$NEW_VERSION\"/" {} \;
+find $PACKAGES -name Cargo.toml -type f -exec sed -i -e "s/^\(bpf-sys.*version = \)\"[^\"]*\"/\\1\"$NEW_VERSION\"/" {} \;
+find $PACKAGES -name Cargo.toml -type f -exec sed -i -e "s/^\(redbpf.*version = \)\"[^\"]*\"/\\1\"$NEW_VERSION\"/" {} \;
