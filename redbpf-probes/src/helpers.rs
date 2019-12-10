c0c5797efc5abb33a8d927947b1e6ecdc554043c
Alessandro Decina <alessandro.d@gmail.com>
2019-12-10 06:25:54 +0000

    probes: helpers: add missing #[inline]s

--- redbpf-probes/src/helpers.rs
+++ redbpf-probes/src/helpers.rs
@@ -7,14 +7,17 @@ mod gen {
 
 pub use gen::*;
 
+#[inline]
 pub fn bpf_get_current_pid_tgid() -> u64 {
     unsafe { gen::bpf_get_current_pid_tgid() }
 }
 
+#[inline]
 pub fn bpf_get_current_uid_gid() -> u64 {
     unsafe { gen::bpf_get_current_uid_gid() }
 }
 
+#[inline]
 pub fn bpf_get_current_comm() -> [c_char; 16] {
     let mut comm: [c_char; 16usize] = [0; 16];
     unsafe { gen::bpf_get_current_comm(&mut comm as *mut _ as *mut c_void, 16u32) };
