b6f2273fc5bd5a77b4d6d589467da5d7ed7855a8
Alessandro Decina <alessandro.d@gmail.com>
2019-12-10 14:10:05 +0000

    cargo-bpf: move to async await

--- cargo-bpf/src/ebpf_io.rs
+++ cargo-bpf/src/ebpf_io.rs
@@ -5,14 +5,16 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use futures::{Async, Poll, Stream};
+use futures::prelude::*;
 use mio::unix::EventedFd;
 use mio::{Evented, PollOpt, Ready, Token};
 use redbpf::PerfMap;
 use std::io;
 use std::os::unix::io::RawFd;
+use std::pin::Pin;
 use std::slice;
-use tokio::reactor::{Handle, PollEvented2};
+use std::task::{Context, Poll};
+use tokio::io::PollEvented;
 
 pub struct GrainIo(RawFd);
 
@@ -43,7 +45,7 @@ impl Evented for GrainIo {
 }
 
 pub struct PerfMessageStream {
-    poll: PollEvented2<GrainIo>,
+    poll: PollEvented<GrainIo>,
     map: PerfMap,
     name: String,
 }
@@ -51,7 +53,7 @@ pub struct PerfMessageStream {
 impl PerfMessageStream {
     pub fn new(name: String, map: PerfMap) -> Self {
         let io = GrainIo(map.fd);
-        let poll = PollEvented2::new_with_handle(io, &Handle::default()).unwrap();
+        let poll = PollEvented::new(io).unwrap();
         PerfMessageStream { poll, map, name }
     }
 
@@ -81,16 +83,15 @@ impl PerfMessageStream {
 
 impl Stream for PerfMessageStream {
     type Item = Vec<Box<[u8]>>;
-    type Error = io::Error;
 
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
         let ready = Ready::readable();
-        if self.poll.poll_read_ready(ready)? == Async::NotReady {
-            return Ok(Async::NotReady);
+        if let Poll::Pending = self.poll.poll_read_ready(cx, ready) {
+            return Poll::Pending;
         }
 
         let messages = self.read_messages();
-        self.poll.clear_read_ready(ready).unwrap();
-        Ok(Async::Ready(Some(messages)))
+        self.poll.clear_read_ready(cx, ready).unwrap();
+        Poll::Ready(Some(messages))
     }
 }
--- cargo-bpf/src/load.rs
+++ cargo-bpf/src/load.rs
@@ -9,8 +9,9 @@ use crate::ebpf_io::PerfMessageStream;
 use crate::CommandError;
 
 use bpf_sys;
-use futures::future::{self, Future};
-use futures::stream::Stream;
+use futures::prelude::*;
+use futures::stream::{self, StreamExt};
+use futures::channel::mpsc;
 use hexdump::hexdump;
 use redbpf::cpus;
 use redbpf::ProgramKind::*;
@@ -20,14 +21,14 @@ use std::fs;
 use std::path::PathBuf;
 use tokio;
 use tokio::runtime::Runtime;
-use tokio_signal::ctrl_c;
+use tokio::signal;
 
 pub fn load(program: &PathBuf, interface: Option<&str>) -> Result<(), CommandError> {
     let data = fs::read(program)?;
     let iface = interface.map(String::from);
     let mut runtime = Runtime::new().unwrap();
     runtime
-        .block_on(futures::lazy(move || {
+        .block_on(async {
             let mut module = Module::parse(&data).expect("failed to parse ELF data");
             for prog in module.programs.iter_mut() {
                 prog.load(module.version, module.license.clone())
@@ -51,32 +52,30 @@ pub fn load(program: &PathBuf, interface: Option<&str>) -> Result<(), CommandErr
                 println!("Loaded: {}, {:?}", prog.name, prog.kind);
             }
             let online_cpus = cpus::get_online().unwrap();
+            let (sender, mut receiver) = mpsc::unbounded();
             for m in module.maps.iter_mut().filter(|m| m.kind == 4) {
                 for cpuid in online_cpus.iter() {
                     let name = m.name.clone();
                     let map = PerfMap::bind(m, -1, *cpuid, 16, -1, 0).unwrap();
-                    let stream = PerfMessageStream::new(m.name.clone(), map);
-                    let fut = stream
-                        .for_each(move |events| {
-                            for event in events {
-                                println!("-- Event: {} --", &name);
-                                hexdump(&event);
-                            }
-                            future::ok(())
-                        })
-                        .map_err(|_| ());
+                    let stream = PerfMessageStream::new(name.clone(), map);
+                    let mut s = sender.clone();
+                    let fut = stream.for_each(move |events| {
+                        s.start_send(Some((name.clone(), events))).unwrap();
+                        future::ready(())
+                    });
                     tokio::spawn(fut);
                 }
             }
+            let mut s = sender.clone();
+            tokio::spawn(signal::ctrl_c().map(move |_| s.start_send(None)));
 
-            ctrl_c()
-                .flatten_stream()
-                .take(1)
-                .into_future()
-                .map(|_| ())
-                .map_err(|_| ())
-        }))
-        .unwrap();
+            while let Some(Some((name, events))) = receiver.next().await {
+                for event in events {
+                    println!("-- Event: {} --", name);
+                    hexdump(&event);
+                }
+            }
+        });
 
     println!("exiting");
 
