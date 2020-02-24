ad81ecfba8be2ce2be81c5545fe2617f14ba8156
Alessandro Decina <alessandro.d@gmail.com>
2020-02-24 09:13:38 +0000

    probes: helpers: add some docs

--- redbpf-probes/src/helpers.rs
+++ redbpf-probes/src/helpers.rs
@@ -1,3 +1,15 @@
+// Copyright 2019-2020 Authors of Red Sift
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+/*!
+Wrappers for the helper functions provided by the BPF subsystem.
+
+See <http://man7.org/linux/man-pages/man7/bpf-helpers.7.html>.
+*/
 use core::mem::{size_of, MaybeUninit};
 
 use crate::bindings::*;
@@ -5,12 +17,16 @@ use cty::*;
 pub use ufmt;
 use ufmt::uWrite;
 pub mod gen {
+/*!
+Low-level, generated bindings for the helper functions provided by the BPF
+subsystem.
+*/
     include!(concat!(env!("OUT_DIR"), "/gen_helpers.rs"));
 }
 
 pub struct TraceMessage {
     msg: [u8; 50],
-    write_i: usize
+    write_i: usize,
 }
 
 impl TraceMessage {
@@ -18,7 +34,7 @@ impl TraceMessage {
     pub fn new() -> Self {
         TraceMessage {
             msg: unsafe { core::mem::MaybeUninit::uninit().assume_init() },
-            write_i: 0
+            write_i: 0,
         }
     }
 
@@ -46,19 +62,24 @@ impl uWrite for TraceMessage {
     }
 }
 
-
 pub use gen::*;
 
+/// Returns the current tgid and pid, encoded as `current_task->tgid << 32 |
+/// current_task->pid`.
 #[inline]
 pub fn bpf_get_current_pid_tgid() -> u64 {
     unsafe { gen::bpf_get_current_pid_tgid() }
 }
 
+/// Returns the current GID and UID, encoded as `current_gid << 32 |
+/// current_uid`.
 #[inline]
 pub fn bpf_get_current_uid_gid() -> u64 {
     unsafe { gen::bpf_get_current_uid_gid() }
 }
 
+/// Returns the `comm` attribute of the current task. The comm attribute contains
+/// the name of the executable (excluding the path) for the current task.
 #[inline]
 pub fn bpf_get_current_comm() -> [c_char; 16] {
     let mut comm: [c_char; 16usize] = [0; 16];
@@ -66,11 +87,14 @@ pub fn bpf_get_current_comm() -> [c_char; 16] {
     comm
 }
 
+/// Returns the time elapsed since system boot, in nanoseconds.
 #[inline]
 pub fn bpf_ktime_get_ns() -> u64 {
     unsafe { gen::bpf_ktime_get_ns() }
 }
 
+// For tracing programs, safely attempt to read `mem::size_of::<T>()` bytes from
+// address src.
 #[inline]
 pub unsafe fn bpf_probe_read<T>(src: *const T) -> Result<T, i32> {
     let mut v: MaybeUninit<T> = MaybeUninit::uninit();
