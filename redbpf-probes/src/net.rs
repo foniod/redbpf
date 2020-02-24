79c84a4d60ace95dd35a94797397c35e57ea9506
Alessandro Decina <alessandro.d@gmail.com>
2020-02-24 09:19:03 +0000

    probes: document redbpf_probes::net

--- redbpf-probes/src/net.rs
+++ redbpf-probes/src/net.rs
@@ -1,3 +1,18 @@
+// Copyright 2019-2020 Authors of Red Sift
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+/*!
+Types and traits for working with networking data.
+
+The main trait exported by this module is `NetworkBuffer`. It's implemented
+by
+[`XdpContext`](https://redsift.github.io/rust/redbpf/doc/redbpf_probes/xdp/struct.XdpContext.html)
+to provide access to the network data.
+ */
 use crate::bindings::*;
 use core::mem;
 use core::slice;
