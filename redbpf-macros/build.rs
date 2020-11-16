e2a6c233ad14d9ec5472882d7769ed299b8986fe
Kevin K <kbknapp@gmail.com>
2020-11-16 15:55:49 -0500

    checks rustc channel at compile time
    This commit uses the `rustc_version` crate to check the Rust channel at
    compile time and set `rustc` cfg flags depending appropriately. This
    allows later checking if we are running on a nightly compiler or not,
    and if so we can enable features that are nightly only (such as emitting
    a compile time warning on use of deprecated proc macro).
    
    Relates to #105

---
+++ redbpf-macros/build.rs
@@ -0,0 +1,20 @@
+extern crate rustc_version;
+use rustc_version::{version_meta, Channel};
+
+fn main() {
+    // Set cfg flags depending on release channel
+    match version_meta().unwrap().channel {
+        Channel::Stable => {
+            println!("cargo:rustc-cfg=RUSTC_IS_STABLE");
+        }
+        Channel::Beta => {
+            println!("cargo:rustc-cfg=RUSTC_IS_BETA");
+        }
+        Channel::Nightly => {
+            println!("cargo:rustc-cfg=RUSTC_IS_NIGHTLY");
+        }
+        Channel::Dev => {
+            println!("cargo:rustc-cfg=RUSTC_IS_DEV");
+        }
+    }
+}
