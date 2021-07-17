2e0a1dd2f2221843e529dc11414c0ae35080fbe0
Junyeong Jeong <rhdxmr@gmail.com>
2021-07-18 00:24:06 +0900

    Add tc example
    Demonstrate how to share a map between tc BPF program and redBPF
    userspace program.
    
    Signed-off-by: Junyeong Jeong <rhdxmr@gmail.com>

---
+++ examples/example-probes/src/tc_map_share/main.rs
@@ -0,0 +1,109 @@
+#![no_std]
+#![no_main]
+/// tc map sharing example. This program is loaded by `tc filter add ...`
+/// command which is executed by `cargo run --example tc-map-share <port>...`
+use core::{
+    mem::{self, MaybeUninit},
+    ptr,
+};
+use memoffset::offset_of;
+use redbpf_macros::map;
+use redbpf_probes::tc::prelude::*;
+
+program!(0xFFFFFFFE, "GPL");
+
+/// A section name of tc map should be `maps`. You can define up to 64 tc maps
+/// in the section (`tc` sets that constraints).
+///
+/// key = port, value = blocked packet count
+#[map(link_section = "maps")]
+static mut blocked_packets: TcHashMap<u16, u64> =
+    TcHashMap::<u16, u64>::with_max_entries(1024, TcMapPinning::GlobalNamespace);
+
+/// BPF program type is BPF_PROG_TYPE_SCHED_CLS
+#[tc_action]
+fn block_ports(skb: SkBuff) -> TcActionResult {
+    let tcp_hdr_offset = match u32::from_be(unsafe { *skb.skb }.protocol << 16) {
+        ETH_P_IP => {
+            let mut uninit = MaybeUninit::<iphdr>::uninit();
+            if unsafe {
+                bpf_skb_load_bytes(
+                    skb.skb as *const _,
+                    mem::size_of::<ethhdr>() as u32,
+                    uninit.as_mut_ptr() as *mut _,
+                    mem::size_of::<iphdr>() as u32,
+                )
+            } < 0
+            {
+                return Ok(TcAction::Ok);
+            }
+            let ipv4_hdr = unsafe { uninit.assume_init() };
+            if !(ipv4_hdr.version() == 4 && IPPROTO_TCP == ipv4_hdr.protocol as u32) {
+                return Ok(TcAction::Ok);
+            }
+            let iphdr_len = ipv4_hdr.ihl() as usize * 4;
+            mem::size_of::<ethhdr>() + iphdr_len
+        }
+        ETH_P_IPV6 => {
+            // following chain of extension headers is not implemented here
+            let nexthdr =
+                skb.load::<u8>(mem::size_of::<ethhdr>() + offset_of!(ipv6hdr, nexthdr))?;
+            if IPPROTO_TCP != nexthdr as u32 {
+                return Ok(TcAction::Ok);
+            }
+            mem::size_of::<ethhdr>() + mem::size_of::<ipv6hdr>()
+        }
+        _ => return Ok(TcAction::Ok),
+    };
+    let port = skb.load::<u16>(tcp_hdr_offset + offset_of!(tcphdr, dest))?;
+    if let Some(cnt) = unsafe { blocked_packets.get_mut(&port) } {
+        trace_print(b"blocked port: ", port);
+        *cnt += 1;
+        Ok(TcAction::Shot)
+    } else {
+        trace_print(b"passed port: ", port);
+        Ok(TcAction::Ok)
+    }
+}
+
+fn hex_u8(v: u8, buf: &mut [u8]) {
+    let w = v / 0x10;
+    buf[0] = if w < 0xa { w + b'0' } else { w - 0xa + b'a' };
+    let u = v % 0x10;
+    buf[1] = if u < 0xa { u + b'0' } else { u - 0xa + b'a' };
+}
+
+fn hex_bytes(arr: &[u8], buf: &mut [u8]) -> usize {
+    let mut pos = 0;
+    for (i, b) in arr.iter().enumerate() {
+        if i != 0 {
+            buf[pos] = b' ';
+            pos += 1;
+        }
+        hex_u8(*b, &mut buf[pos..pos + 2]);
+        pos += 2;
+    }
+    pos
+}
+
+fn trace_print<T>(msg: &[u8], x: T) {
+    let mut buf = [0u8; 128];
+    let mut pos = 0;
+    for c in msg {
+        buf[pos] = *c;
+        pos += 1;
+    }
+
+    let ptr = &x as *const T as *const usize as usize;
+    let sz = mem::size_of::<T>();
+    let mut arr = [0u8; 64];
+    for i in 0..sz {
+        arr[i] = unsafe { ptr::read((ptr + i) as *const usize as *const u8) };
+    }
+
+    pos += hex_bytes(&arr[..sz], &mut buf[pos..]);
+    buf[pos] = b'\n';
+    pos += 2;
+
+    bpf_trace_printk(&buf[..pos]);
+}
---
+++ examples/example-userspace/examples/tc-map-share.rs
@@ -0,0 +1,122 @@
+/// This example demonstrates how to interact with BPF program loaded by tc
+/// utility. A BPF program that is loaded by tc also can get values from or set
+/// values to BPF maps. And tc pins maps to specific paths. Maps are pinned to
+/// `/sys/fs/bpf/tc/globals/<map symbol name>`. In userspace, we can load pinned
+/// BPF maps using `Map::from_pin_file` function. By this method, our userspace
+/// programs are able to interact with BPF programs loaded by tc utility.
+///
+/// Usage: sudo -Es (which cargo) run --example tc-map-share 8080 8081 8082
+///
+/// And try running `nc localhost 8080`. nc will not succeed to connect to 8080
+/// because tc BPF program blocks packets of which port number is 8080
+use redbpf::{HashMap, Map};
+use std::{
+    env, fs,
+    process::{self, Command},
+    time::Duration,
+};
+use tokio::{select, signal::ctrl_c, time::sleep};
+use tracing::{debug, error, Level};
+use tracing_subscriber::FmtSubscriber;
+
+const TC_BLOCKED_PACKETS_MAP: &str = "/sys/fs/bpf/tc/globals/blocked_packets";
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+    if unsafe { libc::getuid() != 0 } {
+        error!("You must be root to use eBPF!");
+        process::exit(1);
+    }
+
+    let ports = env::args()
+        .skip(1)
+        .map(|s| {
+            s.parse::<u16>()
+                .expect(format!("Unable to convert {} to u16", s).as_str())
+        })
+        .collect::<Vec<u16>>();
+    if ports.is_empty() {
+        error!("Specify port numbers to block");
+        process::exit(1);
+    }
+
+    let bpf_elf = concat!(
+        env!("OUT_DIR"),
+        "/target/bpf/programs/tc-map-share/tc-map-share.elf"
+    );
+    // remove .BTF.ext and .eh_frame in order to remove .text
+    // remove .text section because tc filter does not work if .text exists
+    // remove .BTF section because it contains invalid names of BTF types
+    // (currently kernel only allows valid symbol names of C)
+    for sec in &[".BTF.ext", ".eh_frame", ".text", ".BTF"] {
+        if !Command::new("llvm-strip-12")
+            .arg("--no-strip-all")
+            .arg("--remove-section")
+            .arg(sec)
+            .arg(&bpf_elf)
+            .status()
+            .expect("error on running command llvm-strip-12")
+            .success()
+        {
+            error!("error on removing section `{}' using llvm-strip-12", sec);
+            return;
+        }
+    }
+    let new_clsact = Command::new("tc")
+        .args("qdisc add dev lo clsact".split(" "))
+        .status()
+        .expect("error on tc qdisc add")
+        .success();
+
+    debug!("Attaching tc BPF program to `lo' interface as direct action");
+    Command::new("tc")
+        .args("filter add dev lo ingress bpf direct-action".split(" "))
+        .arg("object-file")
+        .arg(bpf_elf)
+        .args("section tc_action/block_ports".split(" "))
+        .status()
+        .expect("error on tc filter add");
+
+    // Load map from pinned file that is just created by tc
+    let map = Map::from_pin_file(TC_BLOCKED_PACKETS_MAP).expect("error on Map::from_pin_file");
+    let blocked_packets = HashMap::<u16, u64>::new(&map).expect("error on HashMap::new");
+
+    // Set port numbers to block
+    // Then port numbers are read by tc BPF program and it will block packets
+    // of which port numbers are found at the `HashMap`.
+    for port in ports {
+        blocked_packets.set(port, 0);
+    }
+
+    println!("Hit Ctrl-C to quit");
+    println!("port => blocked packet count");
+    loop {
+        select! {
+            _ = sleep(Duration::from_secs(1)) => {}
+            _ = ctrl_c() => break
+
+        }
+        for (port, blocked_cnt) in blocked_packets.iter() {
+            println!("{} => {}", port, blocked_cnt);
+        }
+    }
+
+    if new_clsact {
+        let _ = Command::new("tc")
+            .args("qdisc del dev lo clsact".split(" "))
+            .status();
+    } else {
+        let _ = Command::new("tc")
+            .args("filter del dev lo ingress protocol all pref 49152 bpf direct-action".split(" "))
+            .arg("object-file")
+            .arg(bpf_elf)
+            .args("section tc_action/block_ports".split(" "))
+            .status();
+    }
+
+    let _ = fs::remove_file(TC_BLOCKED_PACKETS_MAP);
+}
