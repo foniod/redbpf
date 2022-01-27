826d118e4bad611a90eb695f173f1be2c99fcf10
Junyeong Jeong <rhdxmr@gmail.com>
2022-01-28 00:56:03 +0900

    Fix build scripts of examples and tools
    Signed-off-by: Junyeong Jeong <rhdxmr@gmail.com>

--- redbpf-tools/build.rs
+++ redbpf-tools/build.rs
@@ -1,22 +1,21 @@
 use std::env;
 use std::path::{Path, PathBuf};
 
+use cargo_bpf::BuildOptions;
 use cargo_bpf_lib as cargo_bpf;
 
 fn main() {
     let cargo = PathBuf::from(env::var("CARGO").unwrap());
     let target = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let probes = Path::new("probes");
+    let package = Path::new("probes");
 
-    cargo_bpf::build(
-        &cargo,
-        &probes,
-        &target.join("target"),
-        Vec::new(),
-    )
-    .expect("couldn't compile probes");
+    let mut buildopt = BuildOptions::default();
+    buildopt.target_dir = target.join("target");
 
-    cargo_bpf::probe_files(&probes)
+    cargo_bpf::build(&cargo, &package, &mut Vec::new(), &buildopt)
+        .expect("couldn't compile probes");
+
+    cargo_bpf::probe_files(&package)
         .expect("couldn't list probe files")
         .iter()
         .for_each(|file| {
