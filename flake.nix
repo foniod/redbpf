5021e1794d46779df7eeb1c4acf6642c744a0832
John Soo <jsoo1@asu.edu>
2022-05-01 10:54:48 -0700

    flake: Correct clang/llvm inputs, add pkg-config to shell.
    Signed-off-by: John Soo <jsoo1@asu.edu>

--- flake.nix
+++ flake.nix
@@ -32,13 +32,13 @@
           root = ./.;
           gitSubmodules = true;
 
-          LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
+          LIBCLANG_PATH = "${pkgs.llvmPackages_14.libclang.lib}/lib";
           KERNEL_SOURCE = "${pkgs.linuxPackages.kernel.dev}/lib/modules/${pkgs.linuxPackages.kernel.version}";
 
           nativeBuildInputs = with pkgs; [
             pkgconfig
-            llvm_13
-            clang_13
+            llvm_14
+            clang_14
           ];
           buildInputs = with pkgs; [
             openssl
@@ -65,13 +65,14 @@
           inputsFrom = [ self.packages.${system}.cargo-bpf ];
           nativeBuildInputs = with pkgs; [
             cargo
+            pkg-config
             rustc
             rust-analyzer
             rustfmt
             clippy
           ];
 
-          LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
+          LIBCLANG_PATH = "${pkgs.llvmPackages_14.libclang.lib}/lib";
         };
       });
 }
